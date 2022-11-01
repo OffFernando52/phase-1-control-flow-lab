50c9c7189133d6079aebf185a798efd5d393ee45
Dereck Cervantes <dereckfer51@gmail.com>
2022-11-01 11:05:06 -0400

    commit tree 7801d0c8cda918bc55619bb0667d060680635f05

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(rideDistance){
 
-function ternaryCheckCity(){
+if (rideDistance <= 400){
+  return "This one is on me!";
+}else if (rideDistance >= 400 && rideDistance <= 2000){
+  return "That will be twenty bucks.";
+}else if (rideDistance >= 2000 && rideDistance <= 2500){
+  return "I will gladly take your thirty bucks.";
+}else {
+  return "No can do."
+}}
+
+
+
+function ternaryCheckCity(city){
+ 
+if (city === NYC){ return "ok ,sounds good."
+}else {"No go."}}
+
   // Write your code here!
-}
+ 
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
